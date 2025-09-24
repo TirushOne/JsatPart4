d46061d78c8016ee63f386c5a33edb0952db8809
Jed McKenna <jedmckenna63@gmail.com>
2025-09-24 14:30:58 +1000

    commit

---
+++ main.js
@@ -0,0 +1,25 @@
+class Movie {
+    static currentMaxMovieId = 0;
+
+    constructor(title, year, rating) {
+        this.movieId = Movie.currentMaxMovieId;
+        Movie.currentMaxMovieId ++;
+        this.rating = rating;
+        this.year = year;
+        this.title = title;
+    }
+}
+
+let movies = [
+    Movie("movie a", 1999, 7),
+    Movie("movie b", 2000, 2),
+    Movie("movie c", 2001, 4),
+    Movie("movie d", 2002, 0),
+    Movie("movie e", 2003, 10),
+    Movie("movie g", 2004, 10),
+    Movie("movie h", 2005, 5),
+    Movie("movie i", 2006, 6),
+    Movie("movie j", 2007, 3),
+    Movie("movie k", 2008, 1),
+    Movie("movie l", 2009, 4)
+];
